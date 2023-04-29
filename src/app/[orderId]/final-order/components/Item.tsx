2b55f2e1912fe85d64e0556c5b045e745a881189
Caio Lucas <lienscarlet@gmail.com>
2023-04-29 19:46:14 -0300

    feat: add currency formatter

--- src/app/[orderId]/final-order/components/Item.tsx
+++ src/app/[orderId]/final-order/components/Item.tsx
@@ -1,14 +1,19 @@
+import { formatCurrency } from '@/utils/currency';
+
 interface ItemProps {
   label: string;
   price: number;
 }
 
 export function Item({ label, price }: ItemProps) {
+  const currency = formatCurrency(price).split('\u00A0');
+  console.log(currency);
+
   return (
     <div className="flex justify-between items-center">
       <p className="text-lg">{label}</p>
       <span>
-        <strong className="text-yellow-500">R$</strong> {price}
+        <strong className="text-yellow-500">{currency[0]}</strong> {currency[1]}
       </span>
     </div>
   );
--- src/app/[orderId]/order/components/Item.tsx
+++ src/app/[orderId]/order/components/Item.tsx
@@ -1,4 +1,5 @@
 import { Controller } from '@/components/Controller';
+import { formatCurrency } from '@/utils/currency';
 
 interface ItemProps {
   name: string;
@@ -6,12 +7,15 @@ interface ItemProps {
 }
 
 export function Item({ name, price }: ItemProps) {
+  const currency = formatCurrency(price).split('\u00A0');
+
   return (
     <div className="flex justify-between items-center">
       <div>
         <p className="text-lg">{name}</p>
         <span>
-          <strong className="text-yellow-500">R$</strong> {price}
+          <strong className="text-yellow-500">{currency[0]}</strong>
+          {currency[1]}
         </span>
       </div>
       <Controller />
