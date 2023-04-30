31b58ab006daa686f967ff12b7c3dc7600c9ef0b
Caio Lucas <lienscarlet@gmail.com>
2023-04-30 18:18:07 -0300

    feat: add register functionalities

--- src/app/[shortId]/page.tsx
+++ src/app/[shortId]/page.tsx
@@ -1,11 +1,49 @@
 'use client';
-import { useParams } from 'next/navigation';
+import Link from 'next/link';
+import { useParams, useRouter } from 'next/navigation';
+import { useCallback } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import * as z from 'zod';
 
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const userSchema = z.object({
+  username: z.string().min(1)
+});
+
+type UserData = z.infer<typeof userSchema>;
 
 export default function Register() {
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<UserData>({
+    resolver: zodResolver(userSchema)
+  });
+
   const { shortId } = useParams();
+  const router = useRouter();
+
+  const onSubmit = useCallback(
+    async (data: UserData) => {
+      const res = await fetch('/api/users/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: data.username })
+      });
+
+      const { userId } = await res.json();
+
+      sessionStorage.setItem('@esfirrinha:userId', userId);
+      router.push(`/${shortId}/order`);
+    },
+    [router, shortId]
+  );
 
   return (
     <main className="flex flex-col items-center justify-center gap-36">
@@ -14,10 +52,29 @@ export default function Register() {
         <p>Código do pedido: {shortId}</p>
       </div>
 
-      <form className="flex flex-col gap-4 w-full">
-        <Input label="Nome:" placeholder="Digite seu nome ou apelido" />
-        <Button title="Fazer pedido" />
-        <Button title="Ver pedido geral" variant="secondary" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 w-full"
+      >
+        <Controller
+          name="username"
+          control={control}
+          render={({ field: { onChange } }) => (
+            <Input
+              label="Nome:"
+              placeholder="Digite seu nome ou apelido"
+              onChange={onChange}
+            />
+          )}
+        />
+        <Button title="Fazer pedido" type="submit" />
+        <Link href={`/${shortId}/final-order`}>
+          <Button
+            title="Ver pedido geral"
+            variant="secondary"
+            disabled={isSubmitting}
+          />
+        </Link>
       </form>
     </main>
   );
