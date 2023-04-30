1c0adda4e85c72e2af058c4c50c249cd41483365
Caio Lucas <lienscarlet@gmail.com>
2023-04-30 15:19:54 -0300

    chore: update prisma database

--- prisma/migrations/20230430170421_create_tables/migration.sql
+++ prisma/migrations/20230430170421_create_tables/migration.sql
@@ -2,19 +2,28 @@
 CREATE TABLE "groups" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "shortId" TEXT NOT NULL,
-    "total" INTEGER NOT NULL
+    "total" INTEGER NOT NULL DEFAULT 0
 );
 
 -- CreateTable
 CREATE TABLE "orders" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "user_id" TEXT NOT NULL,
-    "product_id" TEXT NOT NULL,
     "total" INTEGER NOT NULL,
     "groupId" TEXT,
     CONSTRAINT "orders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "orders_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "orders_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "groups" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "order_products" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "order_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "total" INTEGER NOT NULL,
+    CONSTRAINT "order_products_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "order_products_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -40,3 +49,6 @@ CREATE TABLE "categories" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "groups_shortId_key" ON "groups"("shortId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
