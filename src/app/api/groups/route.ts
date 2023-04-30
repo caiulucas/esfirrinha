1d5163c26cf683bfea128994289afebd97bb2b6a
Caio Lucas <lienscarlet@gmail.com>
2023-04-30 18:16:58 -0300

    feat: add get group api route

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -1,9 +1,38 @@
 import { customAlphabet, urlAlphabet } from 'nanoid';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import prisma from '@/lib/prisma';
 
 const nanoid = customAlphabet(urlAlphabet, 6);
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const shortId = searchParams.get('shortId');
+
+  const group = await prisma.group.findUnique({
+    where: { shortId: shortId as string },
+    include: {
+      orders: {
+        include: {
+          user: true,
+          products: {
+            include: {
+              product: {
+                include: { category: true }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+
+  if (!group) {
+    return NextResponse.json({ error: 'Group not found' }, { status: 404 });
+  }
+
+  return NextResponse.json(group);
+}
 
 export async function POST(request: Request) {
   const body: { username: string } = await request.json();
